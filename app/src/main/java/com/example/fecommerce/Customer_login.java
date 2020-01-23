7166777999ccd0214e9f9dc0fcb8372d020babbf
zafar khan <zafarrnah786@gmail.com>
2020-01-24 00:19:13 +0530

    recycler view added

--- app/src/main/java/com/example/fecommerce/Customer_login.java
+++ app/src/main/java/com/example/fecommerce/Customer_login.java
@@ -17,11 +17,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Customer_login extends AppCompatActivity {
 
     private TextInputLayout email,pass;
     private Button sign;
+    private FirebaseUser mcurrentuser;
+    private DatabaseReference mdata;
     private ProgressDialog mprogress;
     private FirebaseAuth mAuth;
     private String c_pass,c_email;
@@ -61,9 +69,34 @@ public class Customer_login extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
-                            Intent intent=new Intent(Customer_login.this,MainActivity.class);
-                            startActivity(intent);
-                            mprogress.dismiss();
+                            mcurrentuser= FirebaseAuth.getInstance().getCurrentUser();
+                            final String uid = mcurrentuser.getUid();
+                            mdata = FirebaseDatabase.getInstance().getReference().child("Customers").child(uid);
+
+                            mdata.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                    if (dataSnapshot.getChildrenCount()==0){
+                                        mprogress.dismiss();
+                                        mAuth.signOut();
+                                        Toast.makeText(Customer_login.this,"Please login from farmers Login...",Toast.LENGTH_LONG).show();
+                                        startActivity(new Intent(Customer_login.this,MainActivity.class));
+
+                                    }
+                                    else{
+                                        Intent intent=new Intent(Customer_login.this,Customer_Itemview_recyclerview.class);
+                                        startActivity(intent);
+                                        finish();
+                                        mprogress.dismiss();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                    Toast.makeText(Customer_login.this,"Something went wrong....",Toast.LENGTH_SHORT).show();
+                                }
+                            });
 
                         }
                         else {
